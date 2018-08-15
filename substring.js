19595cd62c90408a519324687f4565efd33b6270
francisknight <dfranc15@my.chemeketa.edu>
2018-08-15 15:57:35 -0700

    Update substring.js

--- substring.js
+++ substring.js
@@ -1,11 +1,11 @@
 const myword = "sunny";
 
-//version 1 
+// version 1 
 if (myword.indexOf('sun') !== -1) {
-	console.log('Yes');
+	console.log('Yes 🔥');
 }
 
-//ES6 Version
-if (myword.includes('sun')) {
+// ES6 Version
+if (myword.includes('sun 🔥')) {
 	console.log('Yes');
 }
