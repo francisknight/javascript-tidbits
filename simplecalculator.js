d1dad0ef369bf4383c3b37d88182c9c7a2e17616
francisknight <dfranc15@my.chemeketa.edu>
2018-08-15 16:25:31 -0700

    simple js calculator

---
+++ simplecalculator.js
@@ -0,0 +1,14 @@
+let num1 = 10, num2 = 6;
+
+addition(num1, num2);
+subtraction(num1, num2);
+
+function addition (num1, num2) {
+	let result = num1 + num2;
+	return console.log(result); 
+}
+
+function subtraction (num1, num2) {
+	let result = num1 - num2;
+	return console.log(result);
+}
