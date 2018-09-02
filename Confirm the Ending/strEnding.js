2c20a1c78a769c2334d0e3722a83cb6630015f68
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-09-01 18:03:37 -0700

    Basic Algorithm Scripting: Confirm the Ending

---
+++ Confirm the Ending/strEnding.js
@@ -0,0 +1,12 @@
+// Using ES^ .endsWith() method to check if a string ends with the given target 
+function confirmEnding (str, ending) {
+	if (str.endsWith(ending)) {
+		return console.log('Yep ☀');
+	}
+
+	return console.log('Nah 🌧');
+}
+
+let str = 'here comes the sun';
+confirmEnding( str, 'sun'); // Yep ☀️
+confirmEnding( str, 'rain'); // Nah 🌧
