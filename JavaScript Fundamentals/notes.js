5a590815166e93674a00cad2d2aa29b9760b7cf4
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-10 09:06:04 -0800

    Update notes.js

--- JavaScript Fundamentals/notes.js
+++ JavaScript Fundamentals/notes.js
@@ -10,3 +10,15 @@ let person = {
 }
 
 console.log(person.fullName());
+
+// object Constructor
+var apple = new Object();
+apple.color = 'red';
+apple.shape = 'round';
+
+apple.describe = function(){
+	return 'An apple is the color '+this.color+' and
+	is the shape ' +this.shape;
+}
+
+console.log(apple.describe());
