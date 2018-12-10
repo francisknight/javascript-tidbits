c0438f518187407c9a1a33b21c44cd09475b8bcd
Daidemang Francis <dfranc15@my.chemeketa.edu>
2018-12-10 09:06:25 -0800

    Update notes.js

--- JavaScript Fundamentals/notes.js
+++ JavaScript Fundamentals/notes.js
@@ -17,8 +17,8 @@ apple.color = 'red';
 apple.shape = 'round';
 
 apple.describe = function(){
-	return 'An apple is the color '+this.color+' and
-	is the shape ' +this.shape;
+	return 'An apple is the color '+this.color+' and is the shape ' 
+		+this.shape;
 }
 
 console.log(apple.describe());
