f8077c011232d8e18e2177aa3931bfc34f7d241b
Francis Knight <dfranc15@my.chemeketa.edu>
2021-02-08 15:40:52 -0800

    reorder files

---
+++ User Interaction/project1/app.js
@@ -0,0 +1,24 @@
+// TODO LIST with an array
+// each value in the array is an object
+
+const todos = 
+[
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+];
+
+
+console.log(todos);
