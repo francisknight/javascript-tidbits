a24c317fb82689676ef28ccfebc59109da0a41b4
Francis Knight <dfranc15@my.chemeketa.edu>
2021-02-09 15:23:03 -0800

    finish getElementById

--- User Interaction/project1/app.js
+++ User Interaction/project1/app.js
@@ -2,11 +2,14 @@
 // console.dir(document);
 
 
-// Selector 
+// Selectors 
 
-// Get element by id and modify it 
+// Get element by id and modify it //
 let headertitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+
 console.log(headertitle);
 //headertitle.textContent = 'Hello';  // -- doesn't consider styling 
 //headertitle.innerText = 'Goodbye';
 //headertitle.innerHTML = '<h3>Hello</h3>';
+//header.style.borderBottom = `solid 3px #000`; //--- modify css
