32cb567dc6c2feb53b6b24bdf12303a737573f95
Francis Knight <dfranc15@my.chemeketa.edu>
2021-02-09 15:15:23 -0800

    add(app.js): use getElementById to change header title

--- User Interaction/project1/app.js
+++ User Interaction/project1/app.js
@@ -2,3 +2,10 @@
 // console.dir(document);
 
 
+// Selector 
+
+// Get element by id and modify it 
+let headertitle = document.getElementById('header-title');
+console.log(headertitle);
+headertitle.textContent = 'Hello';
+headertitle.innerText = 'Goodbye';
