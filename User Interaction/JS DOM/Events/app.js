74c9af234e9e4a554681e0ee45119cc6d6f70d1c
Francis Knight <dfranc15@my.chemeketa.edu>
2021-02-14 08:38:40 -0800

    updates

--- User Interaction/JS DOM/Events/app.js
+++ User Interaction/JS DOM/Events/app.js
@@ -2,10 +2,15 @@
 
 let button =document.getElementById(`button`);
 
-function buttonClick()
+function buttonClick(e)
 {
     document.getElementById(`header-title`).innerHTML = `Change`;
     document.querySelector(`#main`).style.backgroundColor = `#f6b352`;
+
+    console.log(e.target);
+    console.log(e.target.id);
+    console.log(e.target.className);
+    console.log(e.target.classList);
 }
 
 button.addEventListener(`click`, buttonClick);
