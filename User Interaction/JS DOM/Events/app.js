ecdbb0f813aaf64154292e1b9dd89e9c043c8e66
Francis Knight <dfranc15@my.chemeketa.edu>
2021-02-14 08:28:54 -0800

    updates

--- User Interaction/JS DOM/Events/app.js
+++ User Interaction/JS DOM/Events/app.js
@@ -1,3 +1,10 @@
 // Interactive JS 
- 
 
+let button = document.getElementsById(`button`);
+
+function buttonClick()
+{
+    document.getElementsById(`header-title`).innerHTML = `Change`;
+}
+
+button.addEventListener(`click`, buttonClick);
